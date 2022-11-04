75ff5ac3d44579a3dae94b9dec0600e48ddd9b11
enty3 <104058678+enty3@users.noreply.github.com>
2022-11-04 15:15:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Import stylesheets
+import './style.css';
+
+
+function nohy (parrots, lions, giraffes){
+let sum=0
+sum= (2 * parrots + 4 * lions + 4 * giraffes)
+
+  return sum
+}
+console.log(nohy(1,1,1))
+               //2,4,4//
+
+
+ function total (array) {
+ let sum = []
+ for (let i = 0; i < array.length; i++) {
+ return sum = sum.push(array[i])
+            
+  }      
+ }
+  console.log(total([5,2,3]))
+
+
+
+  function dailymoney (dayrate){
+  const hodiny= 8
+  return hodiny * dayrate
+  }
+  console.log(dailymoney(120))
+
+
+function daysInBudget(fixedPrice, hodinovka){
+const dennehodin= 8
+let daily= dennehodin * hodinovka
+let dny= Math.floor(fixedPrice / daily)
+return dny
+  }
+console.log(daysInBudget(20000,600))
